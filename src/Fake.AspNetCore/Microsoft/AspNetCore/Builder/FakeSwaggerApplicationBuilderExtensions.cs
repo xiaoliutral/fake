d3f9db3081ae1f95f41df8811931515bf918c577
xiaolipro <2357729423@qq.com>
2025-11-05 17:48:23 +0800

    package：8.0.0-preview18

--- src/Fake.AspNetCore/Microsoft/AspNetCore/Builder/FakeSwaggerApplicationBuilderExtensions.cs
+++ src/Fake.AspNetCore/Microsoft/AspNetCore/Builder/FakeSwaggerApplicationBuilderExtensions.cs
@@ -1,11 +1,15 @@
-﻿namespace Microsoft.AspNetCore.Builder;
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Microsoft.AspNetCore.Builder;
 
 public static class FakeSwaggerApplicationBuilderExtensions
 {
-    public static IApplicationBuilder UseFakeSwagger(this IApplicationBuilder app)
+    public static IApplicationBuilder UseFakeSwagger(this IApplicationBuilder app, Action<SwaggerOptions>? setupAction = null,
+        Action<SwaggerUIOptions>? uiSetupAction = null)
     {
         //启用swagger中间件
-        app.UseSwagger();
+        app.UseSwagger(options => setupAction?.Invoke(options));
 
         // 启用swagger-ui中间件，指定swagger json文件路径
         app.UseSwaggerUI(options =>
@@ -17,6 +21,8 @@ public static class FakeSwaggerApplicationBuilderExtensions
             {
                 options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName);
             }
+            
+            uiSetupAction?.Invoke(options);
         });
 
         return app;
